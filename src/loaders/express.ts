ef7755598decddce53dd8b84fda24e9a2ea74476
moete <moetez.boubakri@esprit.tn>
2021-07-16 13:55:59 +0200

    add user Interface&model

---
+++ src/loaders/express.ts
@@ -0,0 +1,13 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import cors from 'cors';
+
+export default ({ app }: { app: express.Application }) => {
+
+    // The magic package that prevents frontend developers going nuts
+  // Alternate description:
+  // Enable Cross Origin Resource Sharing to all origins by default
+  app.use(cors());
+
+
+}
