d22da183ae9eee0d964b73ffe43e950f781af552
moete <moetez.boubakri@esprit.tn>
2021-07-16 12:57:29 +0200

    fix server file

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,4 @@ import config from './config';
 
 const app = express();
 
-
-app.get('/', (req, res) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
 app.listen(config.port, () => console.log(`server is listening on ${config.port}`));
