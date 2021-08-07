a0952857f9012c5df93ef8caac27e9011b30b067
MohammedAli El Hammi <46784544+DaliHami@users.noreply.github.com>
2021-08-07 14:52:59 +0100

    transaction interface

---
+++ src/interfaces/ITransaction.ts
@@ -0,0 +1,13 @@
+export interface IUser {
+    _id: string;
+    Date: string;
+    User: string;
+    Credit: string;
+    Coins: string;
+  }
+  
+  export interface IUserInputDTO {
+    Date: string;
+    User: string;
+    Coins: string;
+  }
