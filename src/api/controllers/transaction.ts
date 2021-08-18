10343cf05bd62a96244cd036b9ee13c5e75da7e0
MohammedAli El Hammi <46784544+DaliHami@users.noreply.github.com>
2021-08-18 05:07:59 +0100

    controllers and routes verification

--- src/api/controllers/transaction.ts
+++ src/api/controllers/transaction.ts
@@ -6,20 +6,42 @@ const transactionService:any= new Services.TransactionService();
 
 
 const transact = (req:Request, res:Response) => {
-    const user = transactionService.save({
+  try {
+    const transaction = transactionService.save({
       Type: req.body.Type,
       User: req.body.User,
       Credit: req.body.Credit,
       Coins: req.body.Coins,
     });
-}
+    if (transaction.message) {
+      return res.status(400).send(transaction);
+    }
+
+    transaction
+      .then((succ: any) => {
+        res.status(200).send({ message: "Transaction successfully created" });
+      })
+      .catch((err: any) => {
+        console.log(err);
+        res.status(500).send({ message: "Please Verify your information!" });
+      });
+  } catch (err: any) {
+    console.log(err);
+    res.status(500).send({ message: "An error has occurred!" });
+  }
+};
+
 
 const count= async (req:Request, res:Response) => {
-    res.status(200).send({data:await transactionService.count()});
+  try{
+        res.status(200).send({data:await transactionService.count()});
+      }catch(err:any){console.log(err);res.status(500).send({ message: "An error has occurred!" });}
   };
 
   const getAllTransactions= async (req:Request, res:Response) => {
+    try{
     res.status(200).send({data:await transactionService.getAllTransactions()});
+  }catch(err:any){console.log(err);res.status(500).send({ message: "An error has occurred!" });}
   };
 
 export default {
