c09afdc4fc3509665d8f9b08467b79d4252ec8a5
MoetezBoubakri <moetez.boubakri@esprit.tn>
2021-11-20 12:05:58 +0100

    fix merge

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -1,23 +1,22 @@
-import config from '../../config';
-import {  Request, Response } from 'express';
+import config from "../../config";
+import { Request, Response } from "express";
 var jwt = require("jsonwebtoken");
-import  Services from "../../services"
-const fs=require('fs')
+import Services from "../../services";
+const fs = require("fs");
 
 var shortid = require("shortid");
 
-const userService:any=new Services.UserService()
-const roleService:any=new Services.RoleService()
-const expiresIn=86400;
-var bcrypt = require("bcryptjs"); 
+const userService: any = new Services.UserService();
+const roleService: any = new Services.RoleService();
+const expiresIn = 86400;
+var bcrypt = require("bcryptjs");
 var shortid = require("shortid");
 
-const signup = async (req:any, res:Response) => {
-    const userDTO = req.body;
-    let user=await userService.checkDuplicate(userDTO.email,userDTO.username);
-    if(user){
-      
-     /* fs.unlink(req.file.path, (err:any) => {
+const signup = async (req: any, res: Response) => {
+  const userDTO = req.body;
+  let user = await userService.checkDuplicate(userDTO.email, userDTO.username);
+  if (user) {
+    /* fs.unlink(req.file.path, (err:any) => {
         if (err) {
           console.error(err)
           return
@@ -26,107 +25,104 @@ const signup = async (req:any, res:Response) => {
         //file removed
       })
       */
-      return   res.status(400).send({ message: "Failed! Username or Email is already in use!" });
-    }
+    return res
+      .status(400)
+      .send({ message: "Failed! Username or Email is already in use!" });
+  }
   /*  let photo=null
     if(req.file)
       photo=req.file.path
     */
-    user = userService.save({
-      email: userDTO.email,
-      name: userDTO.name,
-      username: userDTO.username,
+  user = userService.save({
+    email: userDTO.email,
+    //  name: userDTO.name,
+    username: userDTO.username,
     //  address: userDTO.address,
-   //   country: userDTO.country,
-      //  photo,
-      // changed code attribute into user
-   //   Code: shortid.generate(),
-      password: userDTO.password
-    });
-    user.then(
-      async (user:any)=>{
+    country: userDTO.country,
+    //  photo,
+    // changed code attribute into user
+    //   Code: shortid.generate(),
+    password: userDTO.password,
+  });
+  user
+    .then(async (user: any) => {
+      const role = await roleService.findOneByName("user");
 
-          const role=await roleService.findOneByName("user")
-          
-          user.roles = [role._id];
-          user.save((err:any) => {
-            if (err) {
-              res.status(500).send({ message:  "Something went wrong!" });
-              return;
-            }
-
-            res.send({ message: "User was registered successfully!" });
-          });
-      }
-    ).catch((err:any)=>{
-
-      console.log(err);res.status(500).send({ message: "Please Verify your information!" });
-    })
-  
-  };
-  
-  const signin = (req:Request, res:Response) => {
-    let {username,isAdmin,password}=req.body
-    
-    userService.findOneByEmailOrUsername(username)
-      .exec((err:any, user:any) => {
+      user.roles = [role._id];
+      user.save((err: any) => {
         if (err) {
-          console.log(err);res.status(500).send({ message: "Something went wrong!" });
+          res.status(500).send({ message: "Something went wrong!" });
           return;
         }
-  
-        if (!user) {
-          return res.status(404).send({ message: "User Not found." });
-        }
-        if(!user.verified){
-          return res.status(400).send({ message: "You need to verify your account." });
-        }
-        if(!user.active){
-          return res.status(400).send({ message: "Your account is suspended." });
-        }
 
-        if(isAdmin  ){
-          const getAdmin=user.roles.filter((role:any)=>role.name === "admin" )
-          if(getAdmin.length == 0)
-            return res.status(401).send({ message: "Unauthorized" });
-        }
-
-  
-        var passwordIsValid = bcrypt.compareSync(
-          password,
-          user.password
-        );
-  
-        if (!passwordIsValid) {
-          return res.status(401).send({
-            accessToken: null,
-            message: "Invalid Info!"
-          });
-        }
-  
-        var token = jwt.sign({ id: user.id }, config.SECRET, {
-          expiresIn // 24 hours
-        });
-  
-        var authorities = [];
-  
-        for (let i = 0; i < user.roles.length; i++) {
-          authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
-        }
-        res.status(200).send({
-          id: user._id,
-          username: user.username,
-          name: user.name,
-          email: user.email,
-          roles: authorities,
-          accessToken: token,
-          expiresIn
-        });
+        res.send({ message: "User was registered successfully!" });
       });
-  };
+    })
+    .catch((err: any) => {
+      console.log(err);
+      res.status(500).send({ message: "Please Verify your information!" });
+    });
+};
 
-  
-  export default {
-    signin,
-    signup
-  }
+const signin = (req: Request, res: Response) => {
+  let { username, isAdmin, password } = req.body;
+
+  userService.findOneByEmailOrUsername(username).exec((err: any, user: any) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ message: "Something went wrong!" });
+      return;
+    }
+
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
+    }
+    if (!user.verified) {
+      return res
+        .status(400)
+        .send({ message: "You need to verify your account." });
+    }
+    if (!user.active) {
+      return res.status(400).send({ message: "Your account is suspended." });
+    }
+
+    if (isAdmin) {
+      const getAdmin = user.roles.filter((role: any) => role.name === "admin");
+      if (getAdmin.length == 0)
+        return res.status(401).send({ message: "Unauthorized" });
+    }
+
+    var passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordIsValid) {
+      return res.status(401).send({
+        accessToken: null,
+        message: "Invalid Info!",
+      });
+    }
+
+    var token = jwt.sign({ id: user.id }, config.SECRET, {
+      expiresIn, // 24 hours
+    });
+
+    var authorities = [];
+
+    for (let i = 0; i < user.roles.length; i++) {
+      authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+    }
+    res.status(200).send({
+      id: user._id,
+      username: user.username,
+      name: user.name,
+      email: user.email,
+      roles: authorities,
+      accessToken: token,
+      expiresIn,
+    });
+  });
+};
+
+export default {
+  signin,
+  signup,
+};
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,4 +1,4 @@
-import { Service } from 'typedi';
+import { Service } from "typedi";
 import { IUser } from "../interfaces/IUser";
 const db = require("../models");
 var bcrypt = require("bcryptjs");
@@ -7,131 +7,124 @@ const EmailModel = db.emailVerification;
 const RoleModel = db.role;
 @Service()
 export default class UserService {
-
-    async checkDuplicate(email:string,username:string){
-       const user = await  UserModel.findOne({$or: [
-        {email: email},
-        {username: username}
-    ]})
-        return user;
-    }
-
-    async save(userBody:IUser){
-        let user = new UserModel({
-            email: userBody.email,
-            name: userBody.name,
-         //   address: userBody.address,
-            //country: userBody.country,
-            username: userBody.username,
-          //  photo: userBody.photo,
-            //Code: userBody.Code,
-            password: bcrypt.hashSync(userBody.password, 8)
-          });
-
-        user=await user.save()
-        if(user){
-          const email=new EmailModel({
-            email:user.email,
-            token:Math.random().toString(36).substr(2)+Math.random().toString(36).substr(2)
-          })
-          email.save()
-        }
-        
-        return user
-    }
-
-    
-    async updateAdmin(_id:any,userBody:IUser){
-      return UserModel.findOneAndUpdate({_id},userBody, {
-        new: true
-      })
+  async checkDuplicate(email: string, username: string) {
+    const user = await UserModel.findOne({
+      $or: [{ email: email }, { username: username }],
+    });
+    return user;
   }
 
-    async update(_id:any,userBody:any){
+  async save(userBody: IUser) {
+    let user = new UserModel({
+      email: userBody.email,
+      //   name: userBody.name,
+      //   address: userBody.address,
+      country: userBody.country,
+      username: userBody.username,
+      //  photo: userBody.photo,
+      //Code: userBody.Code,
+      password: bcrypt.hashSync(userBody.password, 8),
+    });
 
-      return UserModel.findOneAndUpdate({_id},userBody, {
-        new: true
-      })
-    }
-  
-            
-      
-    
-
-    findOneByEmailOrUsername(param:string){
-        return UserModel.findOne(
-          {$or:[ {'email':param}, {'username':param}]})
-            .populate("roles", "-__v")
-    }
-    
-    findOneByEmail(param:string){
-      return UserModel.findOne({'email':param})
-  }
-
-    async count(){
-      const count:Number =await UserModel.countDocuments({})
-      return  count;
-    }
-
-    
-
-    async getLastRegistred(){
-      const role=await RoleModel.findOne({name:"user"})
-      const last =await UserModel.find({roles:role._id}).sort( '-createdAt' )
-      .limit(10)
-      return  last;
-    }
-    
-    async getAllUsers(){
-      const role=await RoleModel.findOne({name:"user"})
-      const last =await UserModel.find({roles:role._id}).sort( '-createdAt' )
-      return  last;
-    }
-
-    
-    async getAllUsersEmails(){
-      const role=await RoleModel.findOne({name:"user"})
-      const last =await UserModel.find({roles:role._id},'email').sort( '-createdAt' )
-      return  last;
-    }
-
-    
-    async getVerfiedUsersEmails(){
-      const role=await RoleModel.findOne({name:"user"})
-      const last =await UserModel.find({roles:role._id,verified:true},'email').sort( '-createdAt' )
-      return  last;
-    }
-
-    
-    async statistcsByCountry(){
-      const stat =await UserModel.aggregate([{
-        $group: {
-            "_id": "$country",
-            "count": { $sum: 1 }
-        }
-    },
-    { $sort: { count: -1 }},
-    { $limit : 3 }])
-      return  stat;
-    }
-
-    async getEmailVerfication(email:string,token:string){
-      const val =await EmailModel.findOne({email,token})
-      return  val;
-    }
-
-  
-    deleteUser(_id:any){
-      return UserModel.deleteOne({ _id });
-    }
-  
-    async deleteEmailVerfication(_id:any){
-      const val =await EmailModel.deleteOne({ _id }, function (err:any) {
-        if (err) return console.log(err);
-        // deleted at most one tank document
+    user = await user.save();
+    if (user) {
+      const email = new EmailModel({
+        email: user.email,
+        token:
+          Math.random().toString(36).substr(2) +
+          Math.random().toString(36).substr(2),
       });
+      email.save();
     }
 
+    return user;
+  }
 
-  
+  async updateAdmin(_id: any, userBody: IUser) {
+    return UserModel.findOneAndUpdate({ _id }, userBody, {
+      new: true,
+    });
+  }
+
+  async update(_id: any, userBody: any) {
+    return UserModel.findOneAndUpdate({ _id }, userBody, {
+      new: true,
+    });
+  }
+
+  findOneByEmailOrUsername(param: string) {
+    return UserModel.findOne({
+      $or: [{ email: param }, { username: param }],
+    }).populate("roles", "-__v");
+  }
+
+  findOneByEmail(param: string) {
+    return UserModel.findOne({ email: param });
+  }
+
+  async count() {
+    const count: Number = await UserModel.countDocuments({});
+    return count;
+  }
+
+  async getLastRegistred() {
+    const role = await RoleModel.findOne({ name: "user" });
+    const last = await UserModel.find({ roles: role._id })
+      .sort("-createdAt")
+      .limit(10);
+    return last;
+  }
+
+  async getAllUsers() {
+    const role = await RoleModel.findOne({ name: "user" });
+    const last = await UserModel.find({ roles: role._id }).sort("-createdAt");
+    return last;
+  }
+
+  async getAllUsersEmails() {
+    const role = await RoleModel.findOne({ name: "user" });
+    const last = await UserModel.find({ roles: role._id }, "email").sort(
+      "-createdAt"
+    );
+    return last;
+  }
+
+  async getVerfiedUsersEmails() {
+    const role = await RoleModel.findOne({ name: "user" });
+    const last = await UserModel.find(
+      { roles: role._id, verified: true },
+      "email"
+    ).sort("-createdAt");
+    return last;
+  }
+
+  async statistcsByCountry() {
+    const stat = await UserModel.aggregate([
+      {
+        $group: {
+          _id: "$country",
+          count: { $sum: 1 },
+        },
+      },
+      { $sort: { count: -1 } },
+      { $limit: 3 },
+    ]);
+    return stat;
+  }
+
+  async getEmailVerfication(email: string, token: string) {
+    const val = await EmailModel.findOne({ email, token });
+    return val;
+  }
+
+  deleteUser(_id: any) {
+    return UserModel.deleteOne({ _id });
+  }
+
+  async deleteEmailVerfication(_id: any) {
+    const val = await EmailModel.deleteOne({ _id }, function (err: any) {
+      if (err) return console.log(err);
+      // deleted at most one tank document
+    });
+  }
 }
