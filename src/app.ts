4479b5381a0527ba8fac774a680d3eba04ff8f38
MohammedAli El Hammi <46784544+DaliHami@users.noreply.github.com>
2021-12-10 05:47:11 +0100

    final prob

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import loadersInit from "./loaders";
 import express = require ('express');
 import config from './config';
 import http = require('http');
-
+import  cors = require('cors');
 async function startServer() {
 
   const app = express();
 
   const server = http.createServer(app);
-  
+  app.use(cors())
   const { io }=await loadersInit({ expressApp: app,server });
 
   app.set('socket',io);
