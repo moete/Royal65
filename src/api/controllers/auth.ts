ef1acc28a54dd57675df0032135de47ba6af787c
MohammedAli El Hammi <46784544+DaliHami@users.noreply.github.com>
2022-02-25 17:12:48 +0100

    trello changes

--- src/api/controllers/auth.ts
+++ src/api/controllers/auth.ts
@@ -196,6 +196,7 @@ const signin = (req: Request, res: Response) => {
       username: user.username,
       name: user.name,
       email: user.email,
+      wallet : user.wallet,
       roles: authorities,
       accessToken: token,
       expiresIn,
