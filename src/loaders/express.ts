5d47b10c8d2f87ffac5d137b6cbb1b570d0fa543
MoetezBoubakri <moetez.boubakri@esprit.tn>
2022-03-05 14:32:57 +0100

    deploy

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,33 +1,25 @@
-import * as express from 'express';
-import * as bodyParser from 'body-parser';
-import cors = require('cors');
-import routes from '../api';
-import config from '../config';
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import cors = require("cors");
+import routes from "../api";
+import config from "../config";
 
 export default ({ app }: { app: express.Application }) => {
-  
-    // The magic package that prevents frontend developers going nuts
+  // The magic package that prevents frontend developers going nuts
   // Alternate description:
   // Enable Cross Origin Resource Sharing to all origins by default
-  app.use(cors(
-  //   /*{
-  //   origin: '*',
-  
-  //   methods: [
-  //     'GET',
-  //     'POST',
-  //   ],
-  
-  //   allowedHeaders: [
-  //     'Content-Type',
-  //   ],
-  // }*/
-  ));
+  app.use(
+    cors({
+      origin: "*",
+
+      methods: ["GET", "POST"],
+
+      allowedHeaders: ["Content-Type"],
+    })
+  );
   app.use(bodyParser.json());
   app.use(config.api.prefix, routes());
-  app.get('/status', (req, res) => {
+  app.get("/status", (req, res) => {
     res.status(200).end();
   });
-
-
-}
+};
